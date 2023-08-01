5efee229f8a15dad449b0c7649ddd5c4cbb8a5d4
unknown <moraes.compras@hotmail.com>
2023-08-01 00:03:48 -0300

    Upload exercicio módulo 5 EBAC

---
+++ modulo_5_versionamento_de_codigo/PrimeiroProjetoJava/src/br/com/mmoraes/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.mmoraes;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!!");
+	}
+
+}
